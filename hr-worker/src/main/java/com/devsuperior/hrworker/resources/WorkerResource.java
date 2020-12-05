9db263037f00efe94cc6ca563abe4c8a7e717b1e
jaderson.brandao <jaderson.brandao@gmail.com>
2020-12-05 01:18:16 -0300

    Project hr-eureka-server created

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.persistence.Access;
 import java.util.List;
 
 @RestController
