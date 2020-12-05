c8441d129b99afc2951db46ea9bf1138264ed14f
jaderson.brandao <jaderson.brandao@gmail.com>
2020-12-04 22:35:21 -0300

    RestTemplate

---
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/entetities/Worker.java
@@ -0,0 +1,43 @@
+package com.devsuperior.hrpayroll.entetities;
+
+import java.io.Serializable;
+
+public class Worker implements Serializable {
+
+    private Long id;
+    private String name;
+    private Double dailyInCome;
+
+    public Worker() {
+    }
+
+    public Worker(Long id, String name, Double dailyInCome) {
+        this.id = id;
+        this.name = name;
+        this.dailyInCome = dailyInCome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getDailyInCome() {
+        return dailyInCome;
+    }
+
+    public void setDailyInCome(Double dailyInCome) {
+        this.dailyInCome = dailyInCome;
+    }
+}
--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/resources/PaymentResource.java
@@ -4,16 +4,20 @@ import com.devsuperior.hrpayroll.entetities.Payment;
 import com.devsuperior.hrpayroll.services.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @RequestMapping(value = "/payments")
 public class PaymentResource {
-    
+
     @Autowired
     private PaymentService service;
-    
+
+
     @GetMapping(value = "/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPaymente(@PathVariable Long workerId, @PathVariable Integer days){
         var payment = this.service.getPaymente(workerId, days);
